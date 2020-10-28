34c610bc9b2cd6ee5e1a318d57b1a72dc302c53e
Joel Chippindale <joel@joelchippindale.com>
2020-10-28 05:31:20 +0000

    Make Deno v1.5.0 compatible
    By explicitly typing imports from third party modules to avoid `Property
    'parse' does not exist on type '{}'` errors when bundling

--- deps.ts
+++ deps.ts
@@ -1,1 +1,2 @@
-export { default as stackTrace } from "https://dev.jspm.io/stack-trace";
+import { default as untypedStackTrace } from "https://dev.jspm.io/stack-trace";
+export const stackTrace: any = untypedStackTrace;
