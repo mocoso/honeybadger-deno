2813a23fc3c8cc1cb742c6fa9b6d6b539e72d40a
Joel Chippindale <joel@joelchippindale.com>
2021-01-14 06:23:14 +0000

    Add support for including meta data in payload
    Initially this is just the context as outlined in honeybadger's
    documentation on notification payloads
    https://docs.honeybadger.io/api/exceptions.html
    
    The API is built to reflect the node client, see
    https://github.com/honeybadger-io/honeybadger-node#honeybadgernotify-report-an-error-to-honeybadger

--- payload_test.ts
+++ payload_test.ts
@@ -1,6 +1,6 @@
 import "./test_deps.ts";
 
-import { assertEquals } from "./deps.ts";
+import { assert, assertEquals } from "./deps.ts";
 
 import { payload } from "./payload.ts";
 
@@ -9,4 +9,13 @@ Deno.test("payload", () => {
 
   assertEquals(example.error.class, "Error");
   assertEquals(example.error.message, "foo");
+});
+
+Deno.test("payload - with context meta data", () => {
+  const metaData = { context: { foo: "bar" } };
+  const example = payload(new Error("foo"), metaData);
+
+  assert(example.request != undefined);
+  assert(example.request.context != undefined);
+  assertEquals(example.request.context, { foo: "bar" });
 });
