ff66735393dbc08a21a0724bde8323a9c82e79a9
Joel Chippindale <joel@monkeysthumb.co.uk>
2022-07-03 16:55:02 +0100

    Switch to https://esm.sh for sourcing packages
    Because esm.sh is recommended for Deno

--- deps.ts
+++ deps.ts
@@ -3,5 +3,5 @@ export {
   assertEquals,
 } from "https://deno.land/std@0.83.0/testing/asserts.ts";
 
-import { default as untypedStackTrace } from "https://dev.jspm.io/stack-trace";
+import { default as untypedStackTrace } from "https://esm.sh/stack-trace@v0.0.10";
 export const stackTrace: any = untypedStackTrace;
