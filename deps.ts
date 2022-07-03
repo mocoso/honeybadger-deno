63107699186f725d9be3b019d86e2fd7173fffb6
Joel Chippindale <joel@monkeysthumb.co.uk>
2022-07-03 16:56:22 +0100

    Update to latest version of testing/asserts package

--- deps.ts
+++ deps.ts
@@ -1,7 +1,7 @@
 export {
   assert,
   assertEquals,
-} from "https://deno.land/std@0.83.0/testing/asserts.ts";
+} from "https://deno.land/std@0.146.0/testing/asserts.ts";
 
 import { default as untypedStackTrace } from "https://esm.sh/stack-trace@v0.0.10";
 export const stackTrace: any = untypedStackTrace;
